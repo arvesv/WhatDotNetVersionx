9c7b50fb99713042aca4770d3f2eae6da44af751
Arve Svendsen <arve.svendsen@gmail.com>
2023-02-02 04:51:01 +0100

    vvv

---
+++ ClassLibraryNET4/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace ClassLibraryNET4
+{
+    public static class Class1
+    {
+        public static string GetRuntimeDotNetVersion()
+        {
+            return "Hello from the a .NET 4.5 assembly\n\n" +
+                   $"Runtime .NET version {AppDomain.CurrentDomain.SetupInformation.TargetFrameworkName}\n" +
+                   $"System version {System.Environment.Version.ToString()}\n";
+        }
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello from the a .NET 4.8 executable\n\n" +
+                              $"Runtime .NET version {AppDomain.CurrentDomain.SetupInformation.TargetFrameworkName}\n" +
+                              $"System version {System.Runtime.InteropServices.RuntimeEnvironment.GetSystemVersion()}\n\n");
+
+            
+
+
+            Console.WriteLine(ClassLibraryNET4.Class1.GetRuntimeDotNetVersion());
+        }
+    }
+
+}
+
